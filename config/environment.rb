82a282d6098b7e9225fcc59e1a371473c33f0dc9
Mike Loseto <losetom1998@gmail.com>
2022-10-10 22:39:45 -0400

    commit tree e12d2c1222cad5df4315adb4abc55d6443bbae49

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
+#ENV["RACK_ENV"] is known as an environment variable
+#This determines whether our code is running in a development environment or a test environment
 
 require_relative "../app/models/artist"
